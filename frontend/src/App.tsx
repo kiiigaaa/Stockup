b663c48efc81942d7ce06fb72add4ce698242e58
kiiigaaa <120770077+kiiigaaa@users.noreply.github.com>
2024-03-06 11:51:58 +0530

    Type narrowing and set search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,30 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Component/CardList/CardList';
 import Search from './Component/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
 
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+  const [serverError, setServerError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  };
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    //setServerError(result.data);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  }
   
   return (
     <div className="App">
